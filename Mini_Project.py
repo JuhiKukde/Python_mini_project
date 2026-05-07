fadbee3ad7f56de1e33fce63f76f093814d2854e
Juhi Kukde <kukadejuhi@gmail.com>
2026-05-07 13:38:23 +0530

    Add files via upload

---
+++ Mini_Project.py
@@ -0,0 +1,40 @@
+# Guess The Number
+
+import random
+
+print(" Welcome to Guess the numbre")
+
+number = random.randint(1,100)
+
+while True:
+    userChoice = input("Guess the Number  or Quite :  ")
+    if(userChoice == "Exit"):
+        break
+
+    userChoice = int(userChoice)
+    if(userChoice == number):
+        print("Success : Corect Guess")
+        break
+    elif(userChoice < number):
+        print(" You number was too small. Take a bigger guess")
+    else:
+        print(" Your number was too big . Take a smaller guess")
+
+print( "-----Game Over ----- ")
+
+
+
+
+# Random Password Generator
+
+import random
+import string
+
+pass_len = 6
+charVal = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(pass_len):
+    password += (random.choice(charVal))
+
+print("Your random password is " ,password )
